f0d092b673044f10c204292fba6f08eaeb73cad8
ManabuFujita <45511233+ManabuFujita@users.noreply.github.com>
2021-04-19 18:32:18 +0900

    update .gitignore

--- lib/Line.php
+++ lib/Line.php
@@ -5,24 +5,22 @@ class Line
   private $id;
   private $url_push;
   private $url_reply;
- 
+
   function __construct()
   {
-    // LineName : test_gomi
-//    $this->access_token = '/W2Q4hSqlkL62yxc1gXAMI+gY/oGAYcaupkg0skA/KWZirH13NbvFWMGtx29V3Yg//vQnFqbxSvRfsBUaAkelCo3LWBEwS3xiUJANfoj9VrCn6BwdhoCculLHDps/o5aNCrunBrVbhLkjRHH9h3w1QdB04t89/1O/w1cDnyilFU=';
     $this->url_push = 'https://api.line.me/v2/bot/message/push';
     $this->url_reply = 'https://api.line.me/v2/bot/message/reply';
   }
-  
+
   public function setToken($token) {
     $this->access_token = $token;
   }
-  
+
   public function setId($id)
   {
     $this->id = $id;
   }
-  
+
   public function hasId()
   {
     return isset($this->id);
@@ -32,7 +30,7 @@ class Line
   {
     return isset($this->access_token);
   }
-  
+
   public function push($message)
   {
     if (!$this->hasId())
@@ -45,7 +43,7 @@ class Line
       print "Access Tokenをセットしていません。<br>";
       return false;
     }
-    
+
     // 送信するメッセージ作成
     $messages = array('type' => 'text',
                       'text' => $message);
@@ -55,33 +53,33 @@ class Line
 
     $this->send($this->url_push, $body);
   }
-  
+
   public function reply($message, $reply_token)
   {
     // 送信するメッセージ作成
     $messages = array('type' => 'text',
                       'text' => $message);
-    
+
     $body = json_encode(array('replyToken' => $reply_token,
                               'messages'   => array($messages)));
                               // 複数送る場合は、array($mesg1,$mesg2) とする。
 
     $this->send($this->url_reply, $body);
-    
+
   }
-  
+
   private function send($url, $body)
   {
     $header = array('Content-Type: application/json',
                     'Authorization: Bearer ' . $this->access_token);
-    
+
     $options = array(CURLOPT_URL            => $url,
                      CURLOPT_CUSTOMREQUEST  => 'POST',
                      CURLOPT_RETURNTRANSFER => true,
                      CURLOPT_HTTPHEADER     => $header,
                      CURLOPT_POSTFIELDS     => $body);
 
-    
+
     print "<pre>";
     print_r($header);
     print "</pre>";
@@ -90,16 +88,16 @@ class Line
     print_r($options);
     print "</pre>";
 
-    
-    
+
+
     $curl = curl_init();
 
     print "<pre>";
     print_r($curl);
     print "</pre>";
 
-    
-    
+
+
     curl_setopt_array($curl, $options);
     curl_exec($curl);
     curl_close($curl);
